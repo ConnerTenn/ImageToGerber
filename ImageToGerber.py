87d027553dcb05de529816a0557e5c703477e248
Conner <connertenn@gmail.com>
2021-08-26 20:13:51 -0400

    Added intial python file
    Some basic option processing

---
+++ ImageToGerber.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import sys
+
+TERM_RESET =   "\033[m"
+TERM_RED =     "\033[1;31m"
+TERM_GREEN =   "\033[1;32m"
+TERM_YELLOW =  "\033[1;33m"
+TERM_BLUE =    "\033[1;34m"
+TERM_MAGENTA = "\033[1;35m"
+TERM_CYAN =    "\033[1;36m"
+TERM_WHITE =   "\033[1;37m"
+
+
+def GetOptions():
+    options = {}
+    argv = iter(sys.argv[1:])
+    for arg in argv:
+        #Config file
+        if arg=="-c":
+            try:
+                options["ConfigFilename"] = next(argv)
+            except:
+                print(TERM_RED+"Error: -c must be followed by a filename"+TERM_RESET)
+        #Default (Last) argument
+        else:
+            options["ImageFilename"] = arg
+
+    return options
+
+options = GetOptions()
+if not "ImageFilename" in options:
+    print(TERM_RED+"Error: An image file must be specified"+TERM_RESET)
+
+
