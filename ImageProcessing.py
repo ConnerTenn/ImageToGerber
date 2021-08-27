Fixed improper progress print calculation

ImageProcessing.py
@@ -32,7 +32,7 @@ def Convolution2d(img, kernel):
         for x in range(img_width):
             piece     = np.resize(img[y:y+kernel_height, x:x+kernel_width], (3,3)) # pads with copies when needed
             out[y, x] = np.sum(kernel * piece)
-        print(F"\033[A{(y/img_height)*100:.2f}%")
+        print(F"\033[A{((y+1)/img_height)*100:.2f}%")
 
     return out
 
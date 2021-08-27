fd997c733b05df9b0c548a8e52a4c0319cf32c5b
Conner <connertenn@gmail.com>
2021-08-27 01:51:11 -0400

    Fixed improper progress print calculation

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -32,7 +32,7 @@ def Convolution2d(img, kernel):
         for x in range(img_width):
             piece     = np.resize(img[y:y+kernel_height, x:x+kernel_width], (3,3)) # pads with copies when needed
             out[y, x] = np.sum(kernel * piece)
-        print(F"\033[A{(y/img_height)*100:.2f}%")
+        print(F"\033[A{((y+1)/img_height)*100:.2f}%")
 
     return out
 
