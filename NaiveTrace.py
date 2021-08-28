fb4e5cb3cdc6fa3c695068219d5ea474f8c5e4fa
Conner <connertenn@gmail.com>
2021-08-28 15:48:42 -0400

    Switched to new modular trace algorithm

--- NaiveTrace.py
+++ NaiveTrace.py
@@ -9,6 +9,131 @@ def Sigmoid(x):
     return 1 / (1 + 2**(-x))
 
 
+"""
+ +----+   +----+
+ |####| 1 |####|
+ |##+-------+##|
+ +--|-+   +-|--+
+  4 |       | 2
+ +--|-+   +-|--+
+ |##+-------+##|
+ |####| 3 |####|
+ +----+   +----+
+"""
+
+LineData = [
+    { 
+        "Kernel":[1,1,
+                  0,0],
+        "Segments":[(2,4)]
+    },
+    { 
+        "Kernel":[0,0,
+                  1,1],
+        "Segments":[(2,4)]
+    },
+    { 
+        "Kernel":[1,0,
+                  1,0],
+        "Segments":[(1,3)]
+    },
+    { 
+        "Kernel":[0,1,
+                  0,1],
+        "Segments":[(1,3)]
+    },
+
+    { 
+        "Kernel":[0,1,
+                  1,1],
+        "Segments":[(1,4)]
+    },
+    { 
+        "Kernel":[1,0,
+                  1,1],
+        "Segments":[(1,2)]
+    },
+    { 
+        "Kernel":[1,1,
+                  0,1],
+        "Segments":[(3,4)]
+    },
+    { 
+        "Kernel":[1,1,
+                  1,0],
+        "Segments":[(2,3)]
+    },
+
+    { 
+        "Kernel":[1,0,
+                  0,0],
+        "Segments":[(1,4)]
+    },
+    { 
+        "Kernel":[0,1,
+                  0,0],
+        "Segments":[(1,2)]
+    },
+    { 
+        "Kernel":[0,0,
+                  1,0],
+        "Segments":[(3,4)]
+    },
+    { 
+        "Kernel":[0,0,
+                  0,1],
+        "Segments":[(2,3)]
+    },
+
+    { 
+        "Kernel":[1,0,
+                  0,1],
+        "Segments":[(1,4),(2,3)]
+    },
+    { 
+        "Kernel":[0,1,
+                  1,0],
+        "Segments":[(1,2),(3,4)]
+    }
+]
+
+def InterpolatePos(a,b):
+    return 1.5-(a+b)
+
+def ConvertToCoords(lineIdx, tl,tr,bl,br):
+    if lineIdx == 1:
+        return [0.5,1]#[InterpolatePos(tl,tr), 1]
+    if lineIdx == 2:
+        return [1,0.5]#[1, InterpolatePos(br,tr)]
+    if lineIdx == 3:
+        return [0.5,0]#[InterpolatePos(bl,br), 0]
+    if lineIdx == 4:
+        return [0,0.5]#[0, InterpolatePos(bl,tl)]
+
+def GenerateLines(x,y, tl,tr,bl,br):
+    lines = []
+    for dat in LineData:
+        kernel = dat["Kernel"]
+
+        if (kernel[0] == (tl>0.5) and
+            kernel[1] == (tr>0.5) and
+            kernel[2] == (bl>0.5) and
+            kernel[3] == (br>0.5)):
+
+            for seg in dat["Segments"]:
+                p1 = ConvertToCoords(seg[0], tl,tr,bl,br)
+                p2 = ConvertToCoords(seg[1], tl,tr,bl,br)
+
+                p1 = (p1[0]+x, p1[1]+y)
+                p2 = (p2[0]+x, p2[1]+y)
+                # print()
+                # print(p1)
+                # print(p2)
+                # exit()
+
+                lines += [ [(p1[0], p2[0]), (p1[1], p2[1])] ]
+    return lines
+
 def LineDetection(img):
     height, width, depth = img.shape
 
@@ -25,7 +150,7 @@ def LineDetection(img):
             b = img[y][x][2]
 
             avg = (r+g+b)/3
-            grayscaleImg[y][x] = Sigmoid(50*(avg-0.5))
+            grayscaleImg[y][x] = Sigmoid(20*(avg-0.5))
         ProgressBar(y,0,height-1)
     print()
 
@@ -33,7 +158,7 @@ def LineDetection(img):
 
     plt.imsave("grayscale.png", img)
 
-    img = img > 0.4
+    # img = img > 0.4
 
 
     print("> Tracing outline")
@@ -47,76 +172,14 @@ def LineDetection(img):
             bl = piece[0][0]
             br = piece[0][1]
 
+            lines += GenerateLines(x,y, tl,tr,bl,br)
 
-            # ++
-            # ..
-            if ((    tl) and (    tr) and (not bl) and (not br)):
-                lines += [ [[x+0.5,x+1.5], [y+1,y+1]] ]
-            # ..
-            # ++
-            if ((not tl) and (not tr) and (    bl) and (    br)):
-                lines += [ [[x+0.5,x+1.5], [y+1,y+1]] ]
-            # +.
-            # +.
-            if ((    tl) and (not tr) and (    bl) and (not br)):
-                lines += [ [[x+1,x+1], [y+0.5,y+1.5]] ]
-            # .+
-            # .+
-            if ((not tl) and (    tr) and (not bl) and (    br)):
-                lines += [ [[x+1,x+1], [y+0.5,y+1.5]] ]
-
-            # .+
-            # ++
-            if ((not tl) and (    tr) and (    bl) and (    br)):
-                lines += [ [[x+0.5,x+1], [y+1,y+1.5]] ]
-            # +.
-            # ++
-            if ((    tl) and (not tr) and (    bl) and (    br)):
-                lines += [ [[x+1,x+1.5], [y+1.5,y+1]] ]
-            # ++
-            # .+
-            if ((    tl) and (    tr) and (not bl) and (    br)):
-                lines += [ [[x+0.5,x+1], [y+1,y+0.5]] ]
-            # ++
-            # +.
-            if ((    tl) and (    tr) and (    bl) and (not br)):
-                lines += [ [[x+1,x+1.5], [y+0.5,y+1]] ]
-
-            # +.
-            # ..
-            if ((    tl) and (not tr) and (not bl) and (not br)):
-                lines += [ [[x+0.5,x+1], [y+1,y+1.5]] ]
-            # .+
-            # ..
-            if ((not tl) and (    tr) and (not bl) and (not br)):
-                lines += [ [[x+1,x+1.5], [y+1.5,y+1]] ]
-            # ..
-            # +.
-            if ((not tl) and (not tr) and (    bl) and (not br)):
-                lines += [ [[x+0.5,x+1], [y+1,y+0.5]] ]
-            # ..
-            # .+
-            if ((not tl) and (not tr) and (not bl) and (    br)):
-                lines += [ [[x+1,x+1.5], [y+0.5,y+1]] ]
-
-            # +.
-            # .+
-            if ((    tl) and (not tr) and (not bl) and (    br)):
-                lines += [ [[x+0.5,x+1], [y+1,y+1.5]] ]
-                lines += [ [[x+1,x+1.5], [y+0.5,y+1]] ]
-            # .+
-            # +.
-            if ((not tl) and (    tr) and (    bl) and (not br)):
-                lines += [ [[x+1,x+1.5], [y+1.5,y+1]] ]
-                lines += [ [[x+0.5,x+1], [y+1,y+0.5]] ]
-
-                # exit()
         ProgressBar(y,0,height-1)
     print()
 
     return lines
 
-img = plt.imread("Test1.png")
+img = plt.imread("Test3.png")
 height, width, depth = img.shape
 
 lines = LineDetection(img)
