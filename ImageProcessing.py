5a335b11e17b794b54cd3d18c479e3430bea6ec2
Wolfies Not Fine <38928196+thehowlinggreywolf@users.noreply.github.com>
2021-08-27 21:07:51 -0400

    Image processing modified (#1)
    * Added .gitignore
    
    * Swapped x and y variables
    
    X relates to horizontal (width)
    Y relates to vertical (height)
    
    Images are stored with lists of rows, meaning the vertical (y) is the outer loop
    
    * output is now .png (No ugly compression)
    
    * Added progress print
    
    * Fixed improper progress print calculation
    
    Co-authored-by: Conner <connertenn@gmail.com>

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -26,11 +26,14 @@ def Convolution2d(img, kernel):
 
     out = np.zeros((img_height, img_width))
 
+    print("> Convolution")
+    print()
     # covolution
-    for x in range(img_height):
-        for y in range(img_width):
-            piece     = np.resize(img[x:x+kernel_height, y:y+kernel_width], (3,3)) # pads with copies when needed
-            out[x, y] = np.sum(kernel * piece)
+    for y in range(img_height):
+        for x in range(img_width):
+            piece     = np.resize(img[y:y+kernel_height, x:x+kernel_width], (3,3)) # pads with copies when needed
+            out[y, x] = np.sum(kernel * piece)
+        print(F"\033[A{((y+1)/img_height)*100:.2f}%")
 
     return out
 
