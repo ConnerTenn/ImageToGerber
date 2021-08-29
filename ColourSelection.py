2863d5d8a59ef7a3f512ec5f34e4d0dba47a34fe
Conner <connertenn@gmail.com>
2021-08-29 04:34:28 -0400

    Improved efficiency of GetColourRepr by using Numpy

--- ColourSelection.py
+++ ColourSelection.py
@@ -41,6 +41,61 @@ def GetColourRepr(pixel):
 
     return R,G,B,V,C,L,H,Sv,Sl,A
 
+#Numpy array version of GetColourRepr
+#Pixels are given as a np.array of each pixel
+def GetColourReprNumpy(pixels):
+    #https://en.wikipedia.org/wiki/HSL_and_HSV
+
+    rgb = pixels[...,:3] #Slice off Alpha channel
+
+    # R,G,B=rgb
+    R = rgb[...,0]
+    G = rgb[...,1]
+    B = rgb[...,2]
+    # V=0 #Value
+    # C=0 #Chroma
+    # L=0 #Lightness
+    H=np.zeros(len(R)) #Hue
+    Sv=np.zeros(len(R)) #Saturation (Value)
+    Sl=np.zeros(len(R)) #Saturation (Lightness)
+    A=np.zeros(len(R)) #Alpha
+
+    # A = pixels[3] if len(pixels)==4 else 1
+    if pixels.shape[1]==4:
+        A = pixels[...,3]
+
+    # maxRGB = max(rgb)
+    # minRGB = min(rgb)
+    maxRGB = np.array(R)
+    maxRGB[G>maxRGB] = G[G>maxRGB]
+    maxRGB[B>maxRGB] = B[B>maxRGB]
+
+    minRGB = np.array(R)
+    minRGB[G<minRGB] = G[G<minRGB]
+    minRGB[B<minRGB] = B[B<minRGB]
+
+    V = np.array(maxRGB)
+    C = V - minRGB 
+    L = (maxRGB+minRGB)/2 
+
+    # if (C==0): pass
+    # elif (R>G and R>B): H = (0 + (G-B)/C)/6
+    # elif (G>R and G>B): H = (2 + (B-R)/C)/6
+    # elif (B>R and B>G): H = (4 + (R-G)/C)/6
+    cond = (C!=0) & (R>G) & (R>B); H[cond] = (0 + (G[cond]-B[cond])/C[cond])/6
+    cond = (C!=0) & (G>R) & (G>B); H[cond] = (2 + (G[cond]-B[cond])/C[cond])/6
+    cond = (C!=0) & (B>R) & (B>G); H[cond] = (4 + (G[cond]-B[cond])/C[cond])/6
+
+    # Sv = (0 if V==0 else C/V)
+    cond = V!=0
+    Sv[cond] = C[cond]/V[cond]
+
+    # Sl = 0 if L==0 or L==1 else (V-L)/min(L,1-L)
+    minL = np.array(L)
+    cond = 1-L < minL; minL[cond] = 1-L[cond]
+    cond = (L!=0) & (L!=1); Sl[cond] = (V[cond]-L[cond])/minL[cond]
+
+    return np.array([R,G,B,V,C,L,H,Sv,Sl,A])
 
 
 
@@ -54,8 +109,8 @@ def TestTolWrap(value, target, posTol, negTol):
     return (value-target)%1 <= posTol or (target-value)%1 <= negTol
 
 #pixel: float[3] range[0,1]
-def SelectPixel(pixel):
-    R,G,B,V,C,L,H,Sv,Sl,A = GetColourRepr(pixel)
+def SelectPixel(colourRepr):
+    R,G,B,V,C,L,H,Sv,Sl,A = colourRepr #GetColourRepr(pixel)
 
     passSelections = False
 
@@ -131,8 +186,8 @@ def TestTolWrapDist(value, target, posTol, negTol):
 
 
 #pixel: float[3] range[0,1]
-def SelectPixelDist(pixel):
-    R,G,B,V,C,L,H,Sv,Sl,A = GetColourRepr(pixel)
+def SelectPixelDist(colourRepr):
+    R,G,B,V,C,L,H,Sv,Sl,A = colourRepr #GetColourRepr(pixel)
 
     selectionDist = 10**100
 
@@ -202,9 +257,15 @@ def SelectImageSections(imagefile, selections, method):
 
     print(F"Image Resolution: {width}x{height} [{depth}]")
     print("> Selecting Colours")
+    #Convert to list of pixels, then pass to GetColourReprNumpy
+    #Returns lists of each colourRepr
+    colourRepr = GetColourReprNumpy(np.reshape(img,(-1,depth)))
     r=0
+    i=0
     for rows in img:
-        for pixel in rows: pixel[...] = selPixelFunc(list(pixel))
+        for pixel in rows:
+            pixel[...] = selPixelFunc(colourRepr[...,i])
+            i+=1
         r+=1
         # print(F"\033[A{(r/height)*100:.2f}%")
         ProgressBar(r, 0, height)
